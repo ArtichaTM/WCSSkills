901beb2cf276ae46c7d1b570e2e13e9f2565dda8
Articha <tima-1324@mail.ru>
2021-11-19 16:18:53 +0300

    Added ability to add skills with delay.

--- plugins/WCSSkills/commands/buttons.py
+++ plugins/WCSSkills/commands/buttons.py
@@ -41,8 +41,9 @@ class Buttons:
     When active button released, calling bind_released
     """
 
-    __slots__ = ('owner', 'buttons', 'skills', 'active_skills', 'hud_update_repeat',
-                 'button_names', 'button_hud1', 'button_hud2', 'skill_hud1', 'skill_hud2')
+    __slots__ = ('owner', 'hud_update_repeat', 'HUD_limit', 'active_buttons',
+                 'buttons', 'button_names', 'skills', 'skills_names'
+                 )
 
     def __init__(self, player):
 
@@ -51,21 +52,16 @@ class Buttons:
 
         # List of buttons, that player has
         self.buttons = []
-
         # List of buttons names
         self.button_names = []
+        # Variable to check if buttons can be used
+        self.active_buttons = 0
+        # Initiating message
 
         # List of skills (with cd), that player has
         self.skills = []
-
-        # Variable to check if buttons can be used
-        self.active_skills = 0
-
-        # Initiating message
-        self.button_hud1 = HudMsg(" ", y=-0.1, channel=20)
-        self.button_hud2 = HudMsg(" ", y=-0.13, channel=21)
-        self.skill_hud1 = HudMsg(" ", y=-0.16, channel=22)
-        self.skill_hud2 = HudMsg(" ", y=-0.19, channel=23)
+        # List of skills names
+        self.skills_names = []
 
         # Initiating repeat function, that update HUD
         self.hud_update_repeat = Repeat(self.hud_update)
@@ -75,37 +71,39 @@ class Buttons:
         Function called when all skills a loaded by WCS_Player
         • Registering for commands
         • Notify player
+        • Creating message for skills with cooldown
         """
 
-        # If player has 0 active abilities - return
-        if len(self.buttons) == 0:
-            self.active_skills = 0
+        # If player has 0 active abilities and 0 delay skills - return
+        if not self.buttons and not self.skills:
+            self.active_buttons = 0
             return
 
         elif len(self.buttons) > 2:
             SayText2(f"\4[WCS]\1 Введено более 2 активных способностей,"
-                     "отмена активных способностей").send(self.owner.index)
-            return
+                     ).send(self.owner.index)
 
-        # Getting skill name
-        name = Skills_info.get_name(self.buttons[0].name)
+        if len(self.buttons) >= 1:
 
-        # Starting hud update. Here u can change how fast
-        self.active_skills = 1
+            # Getting skill name
+            name = Skills_info.get_name(self.buttons[0].name)
 
-        # Registering for commands
-        client_command_manager.register_commands('+ultimate', self.ult_pressed)
-        client_command_manager.register_commands('-ultimate', self.ult_released)
+            # Starting hud update. Here u can change how fast
+            self.active_buttons = 1
 
-        # Player notify
-        if self.owner.data_info['ultimates_activate_notify']:
-            SayText2("\4[WCS]\1 Активирован ультимейт "
-            f"\5{name}\1").send(self.owner.index)
+            # Registering for commands
+            client_command_manager.register_commands('+ultimate', self.ult_pressed)
+            client_command_manager.register_commands('-ultimate', self.ult_released)
 
-        # Adding skill name to list
-        if self.owner.data_info['active_skill_name_length_limit']:
-            self.button_names.append(f"{name:.10}")
-        else: self.button_names.append(name)
+            # Player notify
+            if self.owner.data_info['ultimates_activate_notify']:
+                SayText2("\4[WCS]\1 Активирован ультимейт "
+                f"\5{name}\1").send(self.owner.index)
+
+            # Adding skill name to list
+            if self.owner.data_info['active_skill_name_length_limit']:
+                self.button_names.append(f"{name:.10}")
+            else: self.button_names.append(name)
 
         if len(self.buttons) == 2:
 
@@ -117,7 +115,7 @@ class Buttons:
             client_command_manager.register_commands('-ability', self.abi_released)
 
             # Marking amount of active skills
-            self.active_skills = 2
+            self.active_buttons = 2
 
             # Player notify
             if self.owner.data_info['ultimates_activate_notify']:
@@ -129,7 +127,26 @@ class Buttons:
                 self.button_names.append(f"{name:.10}")
             else: self.button_names.append(name)
 
-        self.hud_update_repeat.start(0.5,execute_on_start=True)
+        if self.skills:
+
+            # Calculating limit
+            limit = 5 - self.active_buttons
+
+            # Limiting self.skills
+            self.skills = self.skills[:limit]
+
+            for skill in self.skills:
+
+                # Setting skill name
+                self.skills_names.append(Skills_info.get_name(skill.name))
+
+
+        # for num, skill in enumerate(self.skills):
+        #     if num > 1: break
+        #
+
+        # Starting HUD repeat
+        self.hud_update_repeat.start(1,execute_on_start=True)
 
     def hud_update(self):
         # User disabled ultimate display?
@@ -138,21 +155,20 @@ class Buttons:
             # Yes, pass ultimate
             pass
 
+        # Is here any active skills?
+        elif not self.buttons: pass
+
         # Checking for cooldown
         elif self.buttons[0].delay.running is True:
 
             # Calculating values for color
             remaining = self.buttons[0].delay.time_remaining
-            elapsed = self.buttons[0].delay.time_elapsed
-            total = self.buttons[0].delay.delay
-            try: xz = elapsed/total
-            except ZeroDivisionError: xz = 0
-            value = int(255*xz)
-            Col = Color(255-value,value,0,255)
+            Col = self.color_calculate(self.buttons[0].delay)
 
             # Displaying ult in HUD
-            self.button_hud1 = HudMsg(f"{self.button_names[0]}: {remaining:.1f}".rjust(10), y=-0.13,
-                              channel=20, color1=Col, hold_time=1.2).send(self.owner.index)
+            HudMsg(f"{self.button_names[0]}: {remaining:.1f}".rjust(10),
+                y=-0.13,  channel=1210, color1=Col, hold_time=1.2).send(self.owner.index)
+
         # CD is passed
         else:
 
@@ -160,37 +176,69 @@ class Buttons:
             Col = Color(0,255,0,255)
 
             # Displaying ult in HUD
-            self.button_hud1 = HudMsg(f"{self.button_names[0]}: готов", y=-0.13,
-                          channel=20, color1=Col, hold_time=1.2).send(self.owner.index)
+            HudMsg(f"{self.button_names[0]}: готов",
+                y=-0.13, channel=1210, color1=Col, hold_time=1.2).send(self.owner.index)
 
         # Is there's an ability, and ability display is ON?
-        if len(self.buttons) == 2 and self.owner.data_info['ability_hud']:
+        if not (len(self.buttons) == 2 and self.owner.data_info['ability_hud']): pass
+
+        # Checking for cooldown
+        elif self.buttons[1].delay.running is True:
+
+            # Calculating values for color
+            remaining = self.buttons[1].delay.time_remaining
+            Col = self.color_calculate(self.buttons[1].delay)
+
+            # Displaying abi in HUD
+            HudMsg(f"{self.button_names[1]}: {remaining:.1f}",
+                y=-0.1, channel=1211, color1=Col, hold_time=1.2
+                   ).send(self.owner.index)
+
+        # Cooldown is passed
+        else:
+
+            # Displaying abi in HUD
+            HudMsg(f"{self.button_names[1]}: готов", y=-0.1,
+                channel=1211, color1=Color(0,255,0,255), hold_time=1.2
+                   ).send(self.owner.index)
+
+        for num, skill in enumerate(self.skills):
+
+            position = self.active_buttons + num
+
+            y_pos = -0.1 - 0.03*position
+            channel = 1212+num
 
             # Checking for cooldown
-            if self.buttons[1].delay.running is True:
+            if skill.cd.running is True:
 
                 # Calculating values for color
-                remaining = self.buttons[1].delay.time_remaining
-                elapsed = self.buttons[1].delay.time_elapsed
-                total = self.buttons[1].delay.delay
-                try: xz = elapsed/total
-                except ZeroDivisionError: return
-                value = int(255*xz)
-                Col = Color(255-value,value,0,255)
+                remaining = skill.cd.time_remaining
+                Col = self.color_calculate(skill.cd)
 
                 # Displaying abi in HUD
-                self.button_hud2 = HudMsg(f"{self.button_names[1]}: {remaining:.1f}",
-                  y=-0.1, channel=21, color1=Col, hold_time=1.2).send(self.owner.index)
+                HudMsg(f"{self.skills_names[num]}: {remaining:.1f}",
+                       y=y_pos, channel=channel, color1=Col, hold_time=1.2
+                       ).send(self.owner.index)
 
             # Cooldown is passed
             else:
 
-                # Color for a ready active skill is constant
-                Col = Color(0,255,0,255)
-
                 # Displaying abi in HUD
-                self.button_hud2 = HudMsg(f"{self.button_names[1]}: готов", y=-0.1,
-                          channel=21, color1=Col, hold_time=1.2).send(self.owner.index)
+                HudMsg(f"{self.skills_names[num]}: готов", y=y_pos,
+                       channel=channel, color1=Color(0, 255, 0, 255), hold_time=1.2
+                       ).send(self.owner.index)
+
+
+
+    @staticmethod
+    def color_calculate(delay) -> Color:
+        elapsed = delay.time_elapsed
+        total = delay.delay
+        try: xz = elapsed/total
+        except ZeroDivisionError: return
+        value = int(255*xz)
+        return Color(255-value,value,0,255)
 
     def ult_pressed(self, _, index):
         if index == self.owner.index:
@@ -226,17 +274,19 @@ class Buttons:
                     self.button_names[num] = Skills_info.get_name(button.name)
 
     def unload(self):
-        if self.active_skills != 0:
-            if self.active_skills >= 1:
+        if self.active_buttons != 0:
+            if self.active_buttons >= 1:
                 client_command_manager.unregister_commands('+ultimate', self.ult_pressed)
                 client_command_manager.unregister_commands('-ultimate', self.ult_released)
-            if self.active_skills >= 2:
+            if self.active_buttons >= 2:
                 client_command_manager.unregister_commands('+ability', self.abi_pressed)
                 client_command_manager.unregister_commands('-ability', self.abi_released)
             self.hud_update_repeat.stop()
             self.buttons.clear()
             self.button_names.clear()
-            self.active_skills = 0
+            self.active_buttons = 0
+
+        self.skills.clear()
 
     def close(self):
         self.unload()
