27082f8afda006640313b73b3be0644915f3aa5a
Articha <tima-1324@mail.ru>
2023-01-28 01:27:23 +0300

    1. Made WCS downloadables optional 2. Moved wcs_logger import during plugin unload out of try CAUTION: when WCS_Logger.py import is faulty, plugin shutdown will be faulty aswell. Players latest (not all) data can be lost

--- plugins/WCSSkills/WCSSkills.py
+++ plugins/WCSSkills/WCSSkills.py
@@ -2,42 +2,71 @@ from contextlib import suppress
 
 WCS_FOLDER = 'WCSSkills'
 
+
 def load():
     from .WCS_Logger import wcs_logger
     wcs_logger('info', 'Plugin is loading')
+
     # Main mod
+
     # Events
     from .events import custom_events
+
     # other_functions
     from . import other_functions
+
     # Admin
     from .admin import admin_events
+
     # WCS_Player
     from . import wcs
+
     # Databases
     from .db import wcs, admin
+
     # Commands
     from .commands import chat
+
     # Radio menu
     from .menus import wcs
+
     # Skills
     from .skills import immune, skill
 
     # Downloadables
     from stringtables.downloads import Downloadables
-    downloads = Downloadables() # Instance
-    downloads.add_directory(f'sound/{WCS_FOLDER}/') # Adding sounds to download list
-    downloads.add_directory(f'materials/models/{WCS_FOLDER}/') # Adding materials download list
-    downloads.add_directory(f'models/{WCS_FOLDER}/') # Adding models download list
+
+    # Instance of wcs downloadables
+    downloads = Downloadables()
+
+    # Sounds folder
+    try:
+        downloads.add_directory(f'sound/{WCS_FOLDER}/')  # Adding sounds to download list
+    except FileNotFoundError:
+        wcs_logger('error', "Couldn't add sound folder to downloadables")
+
+    # Materials
+    try:
+        downloads.add_directory(f'materials/models/{WCS_FOLDER}/')  # Adding sounds to download list
+    except FileNotFoundError:
+        wcs_logger('error', "Couldn't add materials folder to downloadables")
+
+    # Models
+    try:
+        downloads.add_directory(f'models/{WCS_FOLDER}/')  # Adding sounds to download list
+    except FileNotFoundError:
+        wcs_logger('error', "Couldn't add models folder to downloadables")
 
     wcs_logger('info', 'Plugin loaded successfully')
 
+
 def unload():
+
+    from .WCS_Logger import wcs_logger
+    wcs_logger('info', 'Plugin is unloading')
 
     # Making wcs_logger always close
     try:
-        from .WCS_Logger import wcs_logger
-        wcs_logger('info', 'Plugin is unloading')
 
         # Unloading players
         from .wcs.WCSP.wcsplayer import WCS_Players as WCSP
@@ -53,7 +82,7 @@ def unload():
         DC_history.unload_instance()
 
         # Logging plugin unload
-        wcs_logger('info','Plugin unloaded successfully')
+        wcs_logger('info', 'Plugin unloaded successfully')
 
     finally:
         # CLosing wcs_logger
